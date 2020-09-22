ae7c96ca2de778bc0be74df3863e378b66fae0a7
Ebrahim <dearrude@tfwno.gf>
2020-09-22 05:22:21 +0330

    Rapid lint

--- src/changeset.py
+++ src/changeset.py
@@ -1,23 +1,24 @@
-from datetime import datetime, timedelta, timezone
-from khayyam import JalaliDatetime, TehranTimezone
+"""Some modules to work with changesets"""
 import json
-from matplotlib import path as geo_path
-import numpy as np
-import osmapi
-import osmcha.changeset
-from pathlib import Path
+from datetime import datetime, timedelta, timezone
 from os import environ
+from pathlib import Path
 
+import numpy as np
+from khayyam import JalaliDatetime, TehranTimezone
+from matplotlib import path as geo_path
 
-from texts.text_changesets import *
+from texts.text_changesets import translation
 
 
 def gen_border():
+    """Generates border path and bbox out of geojson file"""
+
     # Parse GeoJSON
     country = environ["BORDER"]
     print(f"Border {country} detected")
-    with open(Path.cwd()/"assets"/"borders"/f"{country.lower()}.geojson") as f:
-        lim_border = json.load(f)
+    with open(Path.cwd()/"assets"/"borders"/f"{country.lower()}.geojson") as bor:
+        lim_border = json.load(bor)
     # Shove it into matplotlib
     border_coords = lim_border["features"][0]["geometry"]["coordinates"]
     point_list = np.array(border_coords)
@@ -27,7 +28,10 @@ def gen_border():
     print("Borders path generated")
     return geo_path.Path(border_coords, closed=True), iran_bbox
 
-def in_border(area_coords, border) -> bool:
+
+def bbox_in_border(area_coords, border) -> bool:
+    """Determines whether a bbox is in parsed border or not"""
+
     nodes = [[area_coords["min_lon"], area_coords["min_lat"]],
              [area_coords["min_lon"], area_coords["max_lat"]],
              [area_coords["max_lon"], area_coords["min_lat"]],
@@ -37,31 +41,46 @@ def in_border(area_coords, border) -> bool:
 
 
 def filter_changesets(changesets, border):
+    """Filter changesets that are not in parsed border"""
+
     sel_keys = ["min_lon", "max_lon", "min_lat", "max_lat"]
     for ch_id in list(changesets.keys()):
         info_area = {c_key: changesets[ch_id][c_key] for c_key in sel_keys}
-        if not in_border(info_area, border):
+        if not bbox_in_border(info_area, border):
             del changesets[ch_id]
 
+
 def translate_flags(flag_list) -> list:
+    """Translates flags according to translation dict"""
+
     for idx, flag in enumerate(flag_list):
         flag_list[idx] = translation.get(flag, flag)
     return flag_list
 
+
 def to_teh_time(date_time):
+    """Converts datetime object to Jalali datetime object"""
+
     offset_hour = 4 if JalaliDatetime.now(TehranTimezone()).month <= 6 else 3
     date_time = date_time + timedelta(hours=offset_hour, minutes=30)
     return JalaliDatetime(date_time, TehranTimezone())
 
+
 def query_changesets(api, iran_bbox, border, mins=10) -> dict:
+    """Query latest changesets from osmapi"""
+
     utc_timezone = timezone(timedelta(0))
     time_period = datetime.now(utc_timezone) - timedelta(minutes=mins)
-    changesets = api.ChangesetsGet(**iran_bbox, closed_after=time_period, only_closed=True)
+    changesets = api.ChangesetsGet(
+        **iran_bbox, closed_after=time_period, only_closed=True)
     filter_changesets(changesets, border)
     print(f"{len(changesets)} changsets found")
     return changesets
 
+
 def enhance_detection(change):
+    """Few tricks to enhance flag detection"""
+
     low_imprt = ["New mapper", "User has multiple blocks"]
     in_change = [flag in change.suspicion_reasons for flag in low_imprt]
     if len(change.suspicion_reasons) == 1 and (True in in_change):
--- src/tg-bot.py
+++ src/tg-bot.py
@@ -1,35 +1,42 @@
-from changeset import *
-from pyrogram import Client, filters
-from time import sleep
+"""Implementation of changset.py in Pyrogram for Telegram"""
+
 from apscheduler.schedulers.background import BackgroundScheduler
+from pyrogram import Client, filters
+import osmapi
+import osmcha.changeset
+
+import changeset as cha
 
 app = Client(
     "detective_bot",
-    api_id=environ["API_ID"],
-    api_hash=environ["API_HASH"],
-    bot_token=environ["BOT_TOKEN"]
+    api_id=cha.environ["API_ID"],
+    api_hash=cha.environ["API_HASH"],
+    bot_token=cha.environ["BOT_TOKEN"]
 )
-border, iran_bbox = gen_border()
-bot_commands_dir = Path.cwd() / "src" / "texts" / "bot_commands"
+border, iran_bbox = cha.gen_border()
+bot_commands_dir = cha.Path.cwd() / "src" / "texts" / "bot_commands"
 
 api = osmapi.OsmApi()
 
-gen_channel, sus_channel= environ["GEN_CHANNEL"], environ["SUS_CHANNEL"]
+gen_channel, sus_channel = cha.environ["GEN_CHANNEL"], cha.environ["SUS_CHANNEL"]
+
 
 def changeset_bot(ch_id: int) -> dict:
+    """Gathers latest changests and exports info about them"""
+
     change = osmcha.changeset.Analyse(ch_id)
     change.full_analysis()
-    enhance_detection(change)
+    cha.enhance_detection(change)
     return {
         "id": change.id,
         "uid": f"{change.uid}",
-        "date": to_teh_time(change.date).strftime('%C'),
+        "date": cha.to_teh_time(change.date).strftime('%C'),
         "user": change.user,
-        "comment": translation.get(change.comment, change.comment),
-        "source": translation.get(change.source, change.source),
+        "comment": cha.translation.get(change.comment, change.comment),
+        "source": cha.translation.get(change.source, change.source),
         "user_url": "%20".join(f"https://www.osm.org/user/{change.user}".split()),
         "is_sus": change.is_suspect,
-        "flags": "، ".join(translate_flags(change.suspicion_reasons)),
+        "flags": "، ".join(cha.translate_flags(change.suspicion_reasons)),
         "added": change.create,
         "modified": change.modify,
         "deleted": change.delete,
@@ -41,8 +48,10 @@ def changeset_bot(ch_id: int) -> dict:
 
 
 def chnl_loop():
-    ch_sets = query_changesets(api, iran_bbox, border, interval)
-    for ch_st in ch_sets.keys():
+    """A schedule loop which posts changesets to channels"""
+
+    ch_sets = cha.query_changesets(api, iran_bbox, border, interval)
+    for ch_st in ch_sets:
         ch_info = changeset_bot(ch_st)
         with open(bot_commands_dir / "new_ch.md", "r") as sus_text:
             gen_response = sus_text.read().format(
@@ -50,29 +59,33 @@ def chnl_loop():
                 ch_info["added"], ch_info["modified"], ch_info["deleted"],
                 ch_info["osm_url"], ch_info["osmcha_url"], ch_info["osmviz_url"],
                 ch_info["achavi_url"], ch_info["comment"], ch_info["uid"], ch_info["source"])
-        app.send_message(gen_channel, gen_response, parse_mode="md", disable_web_page_preview=True)
+        app.send_message(gen_channel, gen_response,
+                         parse_mode="md", disable_web_page_preview=True)
         if ch_info["is_sus"]:
             with open(bot_commands_dir / "sus_found.md", "r") as sus_text:
                 sus_response = sus_text.read().format(
-                    ch_info["id"], ch_info["date"], ch_info["user"], ch_info["user_url"], ch_info["added"],
-                    ch_info["modified"], ch_info["deleted"], ch_info["osm_url"],
+                    ch_info["id"], ch_info["date"], ch_info["user"], ch_info["user_url"],
+                    ch_info["added"], ch_info["modified"], ch_info["deleted"], ch_info["osm_url"],
                     ch_info["osmcha_url"], ch_info["osmviz_url"], ch_info["achavi_url"],
                     ch_info["flags"], ch_info["comment"], ch_info["uid"], ch_info["source"])
-            app.send_message(sus_channel, sus_response, parse_mode="md", disable_web_page_preview=True)
+            app.send_message(sus_channel, sus_response,
+                             parse_mode="md", disable_web_page_preview=True)
 
 
 @app.on_message(filters.command(["bebin", "b"]))
-def bebin(client, message):
+def bebin(message):
+    """Query whether a changeset is sus or not; Bot command"""
+
     text = message.text.split(" ")[1:]
-    if not len(text):
+    if len(text) != 0:
         message.reply_text("چیو ببینم؟")
         return
     ch_info = changeset_bot(int(text[0]))
     if ch_info["is_sus"]:
         with open(bot_commands_dir / "is_sus.md", "r") as sus_text:
             response = sus_text.read().format(
-                ch_info["id"], ch_info["date"], ch_info["user"], ch_info["user_url"], ch_info["added"],
-                ch_info["modified"], ch_info["deleted"], ch_info["osm_url"],
+                ch_info["id"], ch_info["date"], ch_info["user"], ch_info["user_url"],
+                ch_info["added"], ch_info["modified"], ch_info["deleted"], ch_info["osm_url"],
                 ch_info["osmcha_url"], ch_info["osmviz_url"], ch_info["achavi_url"],
                 ch_info["flags"], ch_info["comment"])
     else:
@@ -83,15 +96,18 @@ def bebin(client, message):
                 ch_info["osm_url"], ch_info["osmcha_url"], ch_info["osmviz_url"],
                 ch_info["achavi_url"], ch_info["comment"])
         print(response)
-    message.reply_text(response, parse_mode="md", disable_web_page_preview=True)
+    message.reply_text(response, parse_mode="md",
+                       disable_web_page_preview=True)
+
 
 @app.on_message(filters.command(["start"]) & filters.private)
-def start_bot(client, message):
+def start_bot(message):
+    """Bot start reply; Bot command"""
+
     message.reply_text("سلام!")
 
 
-
-interval = 10
+interval = cha.environ["INTERVAL"]
 scheduler = BackgroundScheduler()
 scheduler.add_job(chnl_loop, "interval", minutes=interval)
 
