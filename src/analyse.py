cc1a05ca6d13a8203321a0254508a3d9ec9c2be6
Ebrahim <dearrude@tfwno.gf>
2021-01-28 16:33:39 +0330

    Add Changeset class Add API const to src/const Create fuctions to exctract meta data from changeset Create fuctions to exctract raw data from changeset Revise request function to be modular

--- src/analyse.py
+++ src/analyse.py
@@ -10,35 +10,87 @@ reqs = req.Session()
 class User:
     def __init__(self, user_id):
         self.id = user_id
+        self.__xml = self.__request(OSM_USERS_API, "xml")
+        self.__json = self.__request(MAPBOX_USERS_API, "json")
         self.__osm_api()
         self.__mapbox_api()
 
-    def __osm_api(self):
-        url = OSM_USERS_API.format(user_id=self.id)
+    def __request(self, link: str, type: str):
+        url = link.format(user_id=self.id)
         try:
             request = reqs.get(url)
             request.raise_for_status()
-            user_root = etree.XML(request.content)
+            if type == "xml":
+                return etree.XML(request.content)
+            elif type == "json":
+                return request.json()
         except req.exceptions.HTTPError as e:
             print(e.response.text)
-        self.display_name = user_root.xpath("user/@display_name")[0]
+
+    def __osm_api(self):
+        self.display_name = self.__xml.xpath("user/@display_name")[0]
         self.created_at = datetime.strptime(
-            user_root.xpath("user/@account_created")[0],
+            self.__xml.xpath("user/@account_created")[0],
             "%Y-%m-%dT%H:%M:%SZ")
-        self.chset_count = int(user_root.xpath("user/changesets/@count")[0])
-        self.trace_count = int(user_root.xpath("user/traces/@count")[0])
-        self.blocks = int(user_root.xpath("user/blocks/received/@count")[0])
+        self.chset_count = int(self.__xml.xpath("user/changesets/@count")[0])
+        self.trace_count = int(self.__xml.xpath("user/traces/@count")[0])
+        self.blocks = int(self.__xml.xpath("user/blocks/received/@count")[0])
 
     def __mapbox_api(self):
-        url = MAPBOX_USERS_API.format(user_id=self.id)
+        self.first_edit = datetime.strptime(
+            self.__json["first_edit"],
+            "%Y-%m-%dT%H:%M:%S.%fZ")
+        self.mapping_days = self.__json["extra"]["mapping_days"]
+        self.ch_discussion = self.__json["extra"]["changesets_with_discussions"]
+
+class Changeset:
+    def __init__(self, id: int):
+        self.id = id
+        self.__meta = self.__request(OSM_CH_API)
+        self.raw = self.__request(OSM_CHRAW_API)
+        self.__extract_meta()
+        self.user = User(self.uid)
+        self.__extract_raw()
+
+    def __request(self, link: str):
+        url = link.format(ch_id=self.id)
         try:
             request = reqs.get(url)
             request.raise_for_status()
-            user = request.json()
+            return etree.XML(request.content)
         except req.exceptions.HTTPError as e:
             print(e.response.text)
-        self.first_edit = datetime.strptime(
-            user["first_edit"],
-            "%Y-%m-%dT%H:%M:%S.%fZ")
-        self.mapping_days = user["extra"]["mapping_days"]
-        self.ch_discussion = user["extra"]["changesets_with_discussions"]
+
+    def __extract_meta(self):
+        self.uid = int(self.__meta.xpath("changeset/@uid")[0])
+        self.closed_at = datetime.strptime(
+            self.__meta.xpath("changeset/@closed_at")[0],
+            "%Y-%m-%dT%H:%M:%SZ")
+        self.change_count = int(self.__meta.xpath("changeset/@changes_count")[0])
+        self.bbox = [
+            float(self.__meta.xpath("changeset/@min_lat")[0]),
+            float(self.__meta.xpath("changeset/@min_lon")[0]),
+            float(self.__meta.xpath("changeset/@max_lat")[0]),
+            float(self.__meta.xpath("changeset/@max_lon")[0])
+        ]
+        tag_keys = self.__meta.xpath("changeset/tag/@k")
+        tag_vals = self.__meta.xpath("changeset/tag/@v")
+        self.tags = dict(zip(tag_keys, tag_vals))
+
+    def __extract_raw(self):
+        r = self.raw
+        actions = ["create", "modify", "delete"]
+        self.count_allchange = [r.xpath(f"count({action})")
+            for action in actions]
+        self.count_ref_nodes = [r.xpath(f"count({action}/node[count(tag)=0])")
+            for action in actions]
+        self.count_nodes = [r.xpath(f"count({action}/node[count(tag)>0])")
+            for action in actions]
+        self.count_ways = [r.xpath(f"count({action}/way)")
+            for action in actions]
+        self.count_rels = [r.xpath(f"count({action}/relation)")
+            for action in actions]
+        self.count_highway = [r.xpath(f"count({action}/*/tag[@k='highway']/..)")
+            for action in actions]
+        self.count_building = [r.xpath(f"count({action}/*/tag[@k='building']/..)")
+            for action in actions]
--- src/consts.py
+++ src/consts.py
@@ -1,4 +1,7 @@
 
 ## APIs
+OSM_CH_API = "https://www.openstreetmap.org/api/0.6/changeset/{ch_id}"
+OSM_CHRAW_API = "https://www.openstreetmap.org/api/0.6/changeset/{ch_id}/download"
+
 OSM_USERS_API = 'https://www.openstreetmap.org/api/0.6/user/{user_id}'
 MAPBOX_USERS_API = 'https://osm-comments-api.mapbox.com/api/v1/users/id/{user_id}?extra=true'
